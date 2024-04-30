285d54e1d13596f3d6a39b03ac37472f9eac1a3b
David <davidhenriquehy@gmail.com>
2024-04-30 10:43:07 -0300

    Modal

---
+++ src/Components/Modal/index.jsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { forwardRef, useImperativeHandle, useRef } from "react"
+import styles from './modal.module.css'
+
+
+const Modal = forwardRef(({ children }, ref) => {
+
+    const dialogRef = useRef(null)
+
+    const closeModal = () => {
+        dialogRef.current.close()
+    }
+
+    const openModal = () => {
+        dialogRef.current.showModal()
+    }
+
+
+    useImperativeHandle(ref, () => {
+        return {
+            closeModal,
+            openModal
+        }
+    })
+
+
+    return (
+        <dialog className={styles.dialog} ref={dialogRef}>
+            <header className={styles.header}>
+                <button>
+                    x
+                </button>
+            </header>
+            {children}
+
+        </dialog>
+    )
+
+})
