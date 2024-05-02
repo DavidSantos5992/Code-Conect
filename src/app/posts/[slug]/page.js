448a26d90c345e8be5081483bb298780e2c3e801
David <davidhenriquehy@gmail.com>
2024-05-02 09:07:42 -0300

    estilização commentlist

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -6,38 +6,42 @@ import styles from './page.module.css'
 import { CardPost } from "@/components/CardPost";
 import db from "../../../../prisma/db";
 import { redirect } from "next/navigation";
-
+import { CommentList } from "@/components/CommentList";
 
 async function getPostBySlug(slug) {
 
     try {
         const post = await db.post.findFirst({
-            where: { 
+            where: {
                 slug
             },
             include: {
                 author: true,
-                comments: true
+                comments: {
+                    include: {
+                        author: true
+                    }
+                }
             }
         })
 
         if (!post) {
             throw new Error(`Post com o slug ${slug} não foi encontrado`)
         }
-    
+
         const processedContent = await remark()
             .use(html)
             .process(post.markdown);
         const contentHtml = processedContent.toString();
-    
+
         post.markdown = contentHtml
-    
+
         return post
     } catch (error) {
         logger.error('Falha ao obter o post com o slug: ', {
             slug,
             error
-        })   
+        })
     }
     redirect('/not-found')
 }
@@ -49,6 +53,10 @@ const PagePost = async ({ params }) => {
         <h3 className={styles.subtitle}>Código:</h3>
         <div className={styles.code}>
             <div dangerouslySetInnerHTML={{ __html: post.markdown }} />
+        </div>
+        <div>
+            <h2>Comentarios</h2>
+            <CommentList comments={post.comments} />
         </div>
     </div>)
 }
